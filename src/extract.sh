aa2fda31d4d87cb692b273e48fce9d628d9b751d
Dmitri Wamback <postwarpoem166@gmail.com>
2022-10-30 18:01:29 -0400

    .

--- src/extract.sh
+++ src/extract.sh
@@ -2,8 +2,8 @@
 # to bypass the program from a locked key
 
 gcc src/key.c -o src/key
-rm src/key.asm
-objdump -d src/key >> src/key.asm
+rm src/asm/key_raw.asm
+objdump -d src/key >> src/asm/key_raw.asm
 
 # setne found on line 15 of key.asm should be changed to sete
 # setne (set not equal)
